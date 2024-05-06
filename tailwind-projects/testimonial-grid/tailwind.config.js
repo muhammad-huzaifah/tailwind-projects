691e17d3b2f6d1f94442ee65276f4c8e32177d34
ujebb <ujebbawas@yahoo.com>
2024-05-06 16:20:02 +0700

    update project testimonial-grid 6 Mei 2024

---
+++ tailwind-projects/testimonial-grid/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1024px',
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
