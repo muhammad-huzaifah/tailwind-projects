d35af84c95e99cb5b509e9852e413ccf2dea713a
muhammad-huzaifah <huzaifah2009@gmail.com>
2024-05-10 09:30:41 +0700

    add fylo 10 Mei 2024

---
+++ tailwind-projects/fylo/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
+      },
+    },
+  },
+  plugins: [],
+}
